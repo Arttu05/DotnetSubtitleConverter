29df9fedae00dbbe1442b78237e85bab332eb392
ExplodingSoap <arttu05.vesterinen@gmail.com>
2024-10-23 23:31:57 +0300

    Added .gitignore and start of the project

---
+++ DotnetSubtitleConverter/SubtitleConverter.cs
@@ -0,0 +1,49 @@
+﻿namespace DotnetSubtitleConverter
+{
+	public static class SubtitleConverter
+	{
+		public enum SubtitleType
+		{
+			SRT,
+			VTT
+		}
+
+		public static bool ConvertTo(string filePath, SubtitleType subtitleType)
+		{
+			if (File.Exists(filePath) == false)
+			{
+				throw new FileNotFoundException($"File \"{filePath}\" not found");
+			}
+
+			Stream fileStream = GetFileStream(filePath);
+			SubtitleType inputSubtitleType = GetSubtitleType(fileStream);
+
+
+			return true;
+		}
+
+		public static SubtitleType GetSubtitleType(string filePath)
+		{
+			throw new NotImplementedException();
+		}
+
+		private static SubtitleType GetSubtitleType(Stream filePath)
+		{ 
+			throw new NotImplementedException();
+			
+		}
+
+		// private functions
+		private static Stream GetFileStream(string filePath)
+		{
+			if (File.Exists(filePath) == false)
+			{
+				throw new FileNotFoundException($"File \"{filePath}\" not found");
+			}
+
+			StreamReader sr = new StreamReader(filePath);
+
+			return sr.BaseStream;
+		}
+	}
+}
---
+++ DotnetSubtitleConverter/SubtitleData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetSubtitleConverter
+{
+	internal class SubtitleData
+	{
+
+		public required DateTime time;
+		public required string subtitleContent;
+		public int subNum; //number above the timestamp in SRT subtitles
+
+	}
+}
