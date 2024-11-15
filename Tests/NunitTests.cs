21527a1210b338e1d30777e128240a2e3ec38829
unknown <arttu05.vesterinen@gmail.com>
2024-11-15 11:50:31 +0200

    added Nunit test framework to the project

---
+++ Tests/NunitTests.cs
@@ -0,0 +1,21 @@
+namespace NunitTests
+{
+    public class SubtitleTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void SRT_To_VTT()
+        {
+            Assert.Pass();
+        }
+        [Test]
+        public void VTT_To_SRT()
+        {
+            Assert.Pass();
+        }
+    }
+}
